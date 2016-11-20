8d2712d5840a19bf2baafbf39cdaf5828322290c
Natasha <n.e.dolgih@yandex.ru>
2016-11-20 20:13:09 +0500

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   telegram: {
     token: process.env.TELEGRAM_TOKEN || '238471408:AAG55S_j7XegBq1GmEJVJI1BN-sbJpEpzSQ',
-    externalUrl: process.env.CUSTOM_ENV_VARIABLE || '',
+    externalUrl: process.env.CUSTOM_ENV_VARIABLE || 'https://warm-headland-52199.herokuapp.com/',
     port: process.env.PORT || 443,
     host: '0.0.0.0'
   }
